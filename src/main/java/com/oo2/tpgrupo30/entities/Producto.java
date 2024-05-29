f1b0fd9607d81db6c01d9ca7a65101495de809ed
Santi <canosantiago404@gmail.com>
2024-05-29 16:30:10 -0300

    añadidos mas atributos

--- src/main/java/com/oo2/tpgrupo30/entities/Producto.java
+++ src/main/java/com/oo2/tpgrupo30/entities/Producto.java
@@ -1,32 +1,50 @@
 package com.oo2.tpgrupo30.entities;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Setter
 @Getter
-
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "producto")
 public class Producto {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id_producto")
-	private int id_producto;
+	private int idProducto;
+
+	@Column(name = "codigo", unique = true, nullable = false)
+	private String codigo;
+
+	@Column(name = "nombre", nullable = false)
+	private String nombre;
 
 	@Column(name = "description", nullable = false)
 	private String description;
 
-	@Column(name = "costos", nullable = false)
-	private double costos;
+	@Column(name = "costo", nullable = false)
+	private double costo;
 
-	@Column(name = "precioVenta", nullable = false)
-	private double precio_venta;
+	@Column(name = "precio_venta", nullable = false)
+	private double precioVenta;
 
+	@Column(name = "cantidad_en_stock", nullable = false)
+	private int cantidad_en_stock;
+
+	@OneToMany(mappedBy = "producto", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<Lote> lotes;
 }
--- src/main/java/com/oo2/tpgrupo30/entities/Usuario.java
+++ src/main/java/com/oo2/tpgrupo30/entities/Usuario.java
@@ -4,13 +4,15 @@ import java.time.LocalDateTime;
 
 import org.hibernate.annotations.CreationTimestamp;
 
-import jakarta.annotation.Generated;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -19,21 +21,29 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "User")
-
 public class Usuario {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id_user")
-	private int id;
-	
+	private int idUsuario;
+
 	@Column(name = "nombre", unique = true, nullable = false)
 	private String nombre;
-	
+
 	@Column(name = "clave", nullable = false)
 	private String clave;
-	
+
 	@Column(name = "createdAt", nullable = false)
 	@CreationTimestamp
 	private LocalDateTime createdAt;
+
+	@Enumerated(EnumType.STRING)
+	@Column(name = "rol", nullable = false)
+	private Rol rol;
+
+	public enum Rol {
+		ADMIN, CLIENTE
+	}
 }
