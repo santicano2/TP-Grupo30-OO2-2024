1b2fdc3ae18feadc60fa8d0dcd27c6f491e1fe06
Santi <canosantiago404@gmail.com>
2024-05-29 16:51:59 -0300

    añadida entidad lote

---
+++ src/main/java/com/oo2/tpgrupo30/entities/Lote.java
@@ -0,0 +1,48 @@
+package com.oo2.tpgrupo30.entities;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "lote")
+public class Lote {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_lote")
+	private int idLote;
+
+	@ManyToOne
+	@JoinColumn(name = "id_producto", nullable = false)
+	private Producto producto;
+
+	@Column(name = "cantidad", nullable = false)
+	private int cantidad;
+
+	@Column(name = "fecha_ingreso", nullable = false)
+	@Temporal(TemporalType.DATE)
+	private LocalDate fechaIngreso;
+
+	@Column(name = "proveedor", nullable = false)
+	private String proveedor;
+
+	@Column(name = "precio_compra", nullable = false)
+	private Double precioCompra;
+}
