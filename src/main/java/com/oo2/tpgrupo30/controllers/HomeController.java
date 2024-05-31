663df518588ec41e89f77dc5ad9c15e0e12a8393
Santi <canosantiago404@gmail.com>
2024-05-31 17:02:44 -0300

    añadido ModelAndView al controlador home para que muestre correctamente el nombre del usuario en index

--- src/main/java/com/oo2/tpgrupo30/controllers/HomeController.java
+++ src/main/java/com/oo2/tpgrupo30/controllers/HomeController.java
@@ -1,5 +1,7 @@
 package com.oo2.tpgrupo30.controllers;
 
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,13 +13,15 @@ import com.oo2.tpgrupo30.helpers.ViewRouteHelper;
 @Controller
 @RequestMapping("/")
 public class HomeController {
-	
+
 	@GetMapping("/index")
 	public ModelAndView index() {
 		ModelAndView modelAndView = new ModelAndView(ViewRouteHelper.INDEX);
-        return modelAndView;
+		User usuario = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		modelAndView.addObject("usuario", usuario.getUsername());
+		return modelAndView;
 	}
-	
+
 	@GetMapping("/")
 	public RedirectView redirectToHomeIndex() {
 		return new RedirectView(ViewRouteHelper.ROUTE_INDEX);
