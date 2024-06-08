0f110bb3bfe00a2f69f934a3d88d44f2c0df0c6b
Matías Ferreira <55554362+Mctias00@users.noreply.github.com>
2024-06-08 20:20:14 -0300

    se agrega rutas para controlar vistas de venta

--- src/main/java/com/oo2/tpgrupo30/helpers/ViewRouteHelper.java
+++ src/main/java/com/oo2/tpgrupo30/helpers/ViewRouteHelper.java
@@ -1,25 +1,26 @@
 package com.oo2.tpgrupo30.helpers;
 
 public class ViewRouteHelper {
-	public final static String INDEX = "home/index";
-	public final static String ROUTE_INDEX = "/index";
+    public final static String INDEX = "home/index";
+    public final static String ROUTE_INDEX = "/index";
 
-	// USER
-	public final static String USER_LOGIN = "user/login";
-	public final static String USER_LOGOUT = "user/logout";
+    // USER
+    public final static String USER_LOGIN = "user/login";
+    public final static String USER_LOGOUT = "user/logout";
 
-	// PRODUCTO
-	public final static String PRODUCT_INDEX = "producto/index";
-	public final static String PRODUCT_FORM = "producto/form";
-	public final static String PRODUCT_NEW = "producto/new";
-	public final static String PRODUCT_UPDATE = "producto/update";
+    // PRODUCTO
+    public final static String PRODUCT_INDEX = "producto/index";
+    public final static String PRODUCT_FORM = "producto/form";
+    public final static String PRODUCT_NEW = "producto/new";
+    public final static String PRODUCT_UPDATE = "producto/update";
 
-	public final static String PRODUCT_ROOT = "/productos/";
-	
-	// LOTE
-    public final static String LOTE_INDEX = "lote/index";
-    public final static String LOTE_FORM = "lote/form";
-    public final static String LOTE_NEW = "lote/new";
-    public final static String LOTE_UPDATE = "lote/update";
-    public final static String LOTE_ROOT = "/lotes/";
+    public final static String PRODUCT_ROOT = "/productos/";
+
+    // LOTE
+    public static final String LOTE_INDEX = "lote/index";
+    public static final String LOTE_LIST = "lote/list";
+    public static final String LOTE_FORM = "lote/form";
+
+    // VENTA
+    public static final String COMPRA_FORM = "compra/form";
 }
